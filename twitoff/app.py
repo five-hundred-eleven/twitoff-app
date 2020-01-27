6683dd33e95b2d7e54caa19460cc5bdfd8b4661e
ekoly <ecowley@protonmail.com>
2020-01-27 18:47:29 -0500

    added tweets

--- twitoff/app.py
+++ twitoff/app.py
@@ -3,7 +3,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 def make_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_db.sqlite'
     db = SQLAlchemy(app)
     return app, db
---
+++ twitoff/models/Tweet.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import datetime
+from twitoff import DB
+from twitoff.models.User import User
+
+class Tweet(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    user_id = DB.Column(DB.Integer, DB.ForeignKey("user.id"), nullable=False)
+    text = DB.Column(DB.Unicode(280), nullable=False)
+    date = DB.Column(DB.DateTime(timezone=False), default=datetime.datetime.utcnow, nullable=False)
+
+    fk_user = DB.ForeignKeyConstraint(['user_id'], ['user.id'])
+    user = DB.relationship("User")
