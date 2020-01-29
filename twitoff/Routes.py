a5c266007a98ff9e125863136642f13c691a3cfa
ekoly <ecowley@protonmail.com>
2020-01-28 23:57:44 -0500

    add functionality for adding new users

--- twitoff/Routes.py
+++ twitoff/Routes.py
@@ -6,14 +6,15 @@
 """
 
 
-from flask import render_template
+from flask import render_template, send_from_directory, request, redirect, flash
 
 from sqlalchemy.orm.exc import NoResultFound
 
 from twitoff import APP, DB
-from twitoff.service import user_service, tweet_service
+from twitoff.service import user_service, tweet_service, twitter_service
 from twitoff.models.User import User
 from twitoff.models.Tweet import Tweet
+from twitoff.forms.AddUser import AddUser
 
 
 @APP.route("/")
@@ -22,8 +23,41 @@ def indexPage():
        Renders the base.html template. 
     """
     users = User.query.all()
-    return render_template("base.html", users=users)
 
+    form_adduser = AddUser()
+    return render_template("index.html", users=users, form_adduser=form_adduser)
+
+
+@APP.route("/user/add", methods=["POST"])
+def addUser():
+    """
+        Handles a post request to get a new user.
+    """
+    flash("loading...")
+    username = request.form["username"]
+    print(f"Request to add user {username}")
+
+    try:
+        user = user_service.getUser(username)
+        flash("User already loaded!")
+        return redirect(f"/user/{username}")
+
+    except NoResultFound:
+        pass
+
+    try:
+
+        user, tweets = twitter_service.loadUser(username)
+        tweets_lst = [tweet for tweet in tweets]
+
+        user_service.addUser(user)
+        tweet_service.addTweets(tweets_lst)
+
+        return redirect(f"/user/{username}")
+
+    except:
+        flash("Error adding user!")
+        return redirect("/")
 
 @APP.route("/user/<username>")
 def userPage(username):
@@ -45,3 +79,8 @@ def userPage(username):
     except Exception as e:
         LOG.info(e)
         return "Unknown error."
+
+@APP.route("/css/<filename>")
+def css(filename):
+    print(f"serving css: {filename}")
+    return send_from_directory("../static/css", filename)
--- twitoff/app.py
+++ twitoff/app.py
@@ -12,5 +12,6 @@ load_dotenv()
 def make_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = config("DATABASE_URL")
+    app.config['SECRET_KEY'] = config("SECRET_KEY")
     db = SQLAlchemy(app)
     return app, db
