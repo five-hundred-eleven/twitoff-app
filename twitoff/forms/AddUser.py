d332ecb19adeb45fa8dd3f549a451a5c67966536
ekoly <ecowley@protonmail.com>
2020-01-29 00:02:57 -0500

    add forms

---
+++ twitoff/forms/AddUser.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+    Contains the form for adding a twitter user.
+"""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class AddUser(FlaskForm):
+    """
+        Contains the form for adding a twitter user.
+    """
+    username = StringField("Twitter Username", validators=[DataRequired()])
+    submit = SubmitField("Add User")
